2ee73d44fe29248dac6e7be881e480817431c6d8
buhari.maiwada <maewada@live.com>
2021-10-06 00:44:19 -0400

    added base.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views import View #View class to handle requests
+from django.views.generic.base import TemplateView
 from django.http import HttpResponse #a class to handle sending a type of response  
 
 # Create your views here.
-class Home(View): # Defining the home view function
-    def get(self, request):
-        return HttpResponse('<h1> Home Page </h1>')
+class Home(TemplateView): # Defining the home view function
+    # def get(self, request):
+    #     return HttpResponse('home.html')
+    template_name = 'home.html'
 
-class About(View):  # Defining the about view function
-    def get (self, request):
-        return HttpResponse('<h1> About the finchCollector </h1>')
+class About(TemplateView):  # Defining the about view function
+    # def get (self, request):
+    #     return HttpResponse('about.html')
+    template_name = 'about.html'
