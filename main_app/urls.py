622143ccc005162d5d87be00c1c6f6d176e3de2d
buhari.maiwada <maewada@live.com>
2021-10-05 23:34:32 -0400

    create home and about templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('route string', name of view function, name="string name of route"),
-    # example
-   
-    #defining the home page
+# path('route string', name of view function, name="string name of route"),
+# example
+#defining the home page
     path('', views.Home.as_view(), name='home'),
+# Define route with a path of about/. 
+# Name the route 'about'.
+# Map the route to a view named views.about.
     path('about/', views.About.as_view(), name='about'),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,10 @@ from django.views import View #View class to handle requests
 from django.http import HttpResponse #a class to handle sending a type of response  
 
 # Create your views here.
-class Home(View):
+class Home(View): # Defining the home view function
     def get(self, request):
-        return HttpResponse('home.html')
-class About(View):
+        return HttpResponse('<h1> Home Page </h1>')
+
+class About(View):  # Defining the about view function
     def get (self, request):
-        return HttpResponse('abount.html')
+        return HttpResponse('<h1> About the finchCollector </h1>')
