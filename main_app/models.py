545d229a8fcf3a48abcd8d28864a64b5ad8cf6b9
buhari.maiwada <maewada@live.com>
2021-10-09 00:49:55 -0400

    modified models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+#import the reverse function
+from django.urls import reverse
 
 # Create your models here.
-
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    adult_size = models.IntegerField()
+    lifespan = models.TextField(max_length=250)
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
 
