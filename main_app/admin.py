f938276a4d0dac60249ca3602d04cb4afce35dc0
buhari.maiwada <maewada@live.com>
2021-10-09 01:29:38 -0400

    odified settings and config admin auth

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+#import the Finch Model from manage.py
+from .models import Finch 
 
 # Register your models here.
+admin.site.register(Finch) #this will add the model to the admin
